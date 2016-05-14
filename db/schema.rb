58b307584409151cc58a0ef29e92a1f5daedac4b
rinykia <rinykia@gmail.com>
2016-05-14 18:47:23 +0300

    Attachment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512083424) do
+ActiveRecord::Schema.define(version: 20160514154116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20160512083424) do
   end
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
+
+  create_table "attachments", force: :cascade do |t|
+    t.string   "file"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "question_id"
+  end
+
+  add_index "attachments", ["question_id"], name: "index_attachments_on_question_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let!(:question) {create :question}
+
   describe 'POST #create' do
 
     let(:question) { create :question }
@@ -25,6 +27,30 @@ RSpec.describe AnswersController, type: :controller do
         post :create, answer: attributes_for(:invalid_answer), question_id: question, format: :js 
         expect(response).to render_template :create
       end
+    end
+  end
+  describe 'PATH #update' do
+    let(:answer) {create(:answer, question: question)}
+
+    it 'assigns the requested answer to @answer' do
+      patch :update, id: answer, question_id: question, answer: attributes_for(:answer), format: :js
+      expect(assigns(:answer)).to eq answer
+    end
+
+    it 'assigns the question' do
+       patch :update, id: answer, question_id: question, answer: attributes_for(:answer), format: :js
+       expect(assigns(:question)).to eq question
+    end
+
+    it 'changes answer attributes' do
+      patch :update, id: answer, question_id: question, answer: {body: 'new body'}, format: :js
+      answer.reload
+      expect(answer.body).to eq 'new body'
+    end
+
+    it 'render update template answer' do
+      patch :update, id: answer, question_id: question, answer: attributes_for(:answer), format: :js
+      expect(response).to render_template :update
     end
   end
 end

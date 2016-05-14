74c641b1a765478e22f6cecd67576e6a31b9f141
rinykia <rinykia@gmail.com>
2016-05-14 18:52:16 +0300

    Question accepts nested attributes for attachments

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Question, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
 
+  it {should accept_nested_attributes_for :attachments}
+
   #it 'validates presense of title' do 
     #expect(Question.new(body: '123')).to_not be_valid
   #end
